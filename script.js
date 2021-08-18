20e91d1aff2454aec542a16bc07a4e7db79f6cc2
superspeeder <andy@delusion.org>
2021-08-17 18:06:11 -0700

    eeeeeee

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var canvas = document.getElementById("canvas")
-canvas.width = document.body.width;
-canvas.height = document.body.height;
+var canvas = document.getElementById("canvas");
+var gl = canvas.getContext('experimental-webgl');
 
-var gl = canvas.getContext('experimental-webgl')
+gl.clearColor(1,0,0,1);
+gl.clear(gl.COLOR_BUFFER_BIT);
 
-gl.clearColor(1,0,0,1)
-gl.clear(gl.COLOR_BUFFER_BIT)
+gl.begin(gl.TRIANGLES);
+gl.color4f(0.0,1.0,1.0,1.0);
+gl.vertex2f(0.0,0.0);
+gl.vertex2f(1.0,0.0);
+gl.vertex2f(0.0,1.0);
+gl.end(gl.TRIANGLES)
+

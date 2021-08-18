275e065ca11e3fe9beec2ede14a93af68bc3cc5b
superspeeder <andy@delusion.org>
2021-08-17 17:53:46 -0700

    e

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas")
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+canvas.width = document.innerWidth;
+canvas.height = document.innerHeight;
 
 var gl = canvas.getContext('experimental-webgl')
 

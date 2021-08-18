ebfe4d50081f29fa647446e7ad205eef1754913c
superspeeder <andy@delusion.org>
2021-08-17 18:15:17 -0700

    fix bg

--- script.js
+++ script.js
@@ -104,17 +104,11 @@ gl.enableVertexAttribArray(coord);
 
 /*=========Drawing the triangle===========*/
 
-// Clear the canvas
-gl.clearColor(0.5, 0.5, 0.5, 0.9);
-
 // Enable the depth test
 gl.enable(gl.DEPTH_TEST);
 
-// Clear the color buffer bit
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-// // Set the view port
-// gl.viewport(0,0,canvas.width,canvas.height);
+// Set the view port
+gl.viewport(0,0,canvas.width,canvas.height);
 
 // Draw the triangle
 gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT,0);
